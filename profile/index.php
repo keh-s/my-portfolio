b7b5d230aa93587795086cfaf35fa90548f9c0d5
keh-s <q8127zoz@yahoo.co.jp>
2020-03-31 23:41:32 +0900

    service-boxにimg追加

--- profile/index.php
+++ profile/index.php
@@ -47,18 +47,18 @@
                     </div>
                     <div class="service-wrapper">
                      <div class="service-box">
-                        <img src="">
+                        <img src="img/pc-image.jpg" class="service-img">
                         <p class="service-ttl">分かりやすいデザイン<br>
 あたらしいデザイン</p>
                         <p class="service-txt">サイト訪問者の目線を考えた、分かりやすく見やすいサイト作り。かつ、お客様とのコミュニケーションを通して、ご希望に沿ったデザインをご提案します。</p>
 </div>
                     <div class="service-box">
-                        <img src="">
+                        <img src="" class="service-img">
                         <p class="service-ttl">丁寧で分かりやすい説明</p>
                          <p class="service-txt">細やかな配慮や柔軟な対応で円滑に制作を進めます。納期やお見積りご相談ください。</p>
 </div>
                     <div class="service-box">
-                        <img src="">
+                        <img src="img/support-image.jpg" class="service-img">
                         <p class="service-ttl">納品後も
 あんしんサポート</p>
                         <p class="service-txt">納品後もご要望を頂ければ継続してサポートいたします。インターネットに詳しくないお客様でも丁寧にアドバイスいたします。</p>
